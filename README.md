94b5e031f97e09c787281d0edd9971536e6a575e
Lani-Dom <dguez.lani@gmail.com>
2024-02-07 23:52:37 -0600

    First iteration refinement

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Vehicles_WebApp
 
-Proyecto relacionado con anuncios de ventas de coches. En dicho registro de anuncios se muestran detalles como el precio, modelo del coche, año, condición, cilindros, transmisión, color, fecha de publicación, etc.
+Proyecto de análisis exploratorio de datos con anuncios de ventas de coches. Se muestra un histograma relacionando el kilometraje y la cantidad de autos, Asímismo se muestra un gráfico de dispersión con la relación del kilometraje y el precio de los autos.
+
+En este registro de anuncios existen detalles como el precio, modelo del coche, año, condición, cilindros, transmisión, color, fecha de publicación, etc.
 
 
 ## Acceso desde Render.com
--- app.py
+++ app.py
@@ -2,31 +2,35 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # read data
+car_data = pd.read_csv('vehicles_us.csv') # Leer datos
 
+# Encabezado
 st.header('Aplicación web - Anuncios de ventas de autos')
-hist_button = st.button('Construir histograma') # crear un botón
 
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+# Botón para histograma
+hist_button = st.button('Construir histograma')
+
+# Si se hace clic en el botón
+if hist_button: 
+    st.write('Histograma para datos de anuncios de venta de coches (relación kilometraje y cantidad)')
     
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    # Histograma
+    fig = px.histogram(car_data, x="odometer") 
 
-    # mostrar un gráfico Plotly interactivo
+    # Despliegue
     st.plotly_chart(fig, use_container_width=True) 
 
 
 
-disp_button = st.button('Construir gráfico de dispersión') # crear un botón
+# Botón para gráfico de dispersión
+disp_button = st.button('Construir gráfico de dispersión') 
 
-if disp_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+# Si se hace clic en el botón
+if disp_button: 
+    st.write('Gráfico de dispersión para datos de anuncios de venta de coches (relación kilometraje y precio)')
     
-    # crear un gráfico de dispersión
+    # Gráfico de dispersión
     fig2 = px.scatter(car_data, x="odometer", y="price") 
 
-    # mostrar un gráfico Plotly interactivo
+    # Despliegue
     st.plotly_chart(fig2, use_container_width=True) 
